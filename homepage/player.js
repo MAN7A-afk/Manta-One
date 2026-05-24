1501f17ed39bb5f187b53d55d79b7fab4c4afe3c
Timotéé <mantastores1@gmail.com>
2026-05-24 21:53:45 +0100

    final commit God abeg

---
+++ homepage/player.js
@@ -0,0 +1,52 @@
+const API_KEY = "8c5359fb9f2b0c63381a972d9ccb880e";
+
+const movieId = 157336;
+
+async function fetchMovie(){
+
+    // FETCH MOVIE INFO
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`
+    );
+
+    const movie = await response.json();
+
+
+
+
+    // DISPLAY MOVIE INFO
+    document.getElementById("movie").innerHTML = `
+        <img 
+            src="https://image.tmdb.org/t/p/original${movie.backdrop_path}"
+        >
+
+        <h1>${movie.title}</h1>
+
+        <p>${movie.overview}</p>
+
+        <a 
+            href="https://www.justwatch.com/us/movie/interstellar"
+            target="_blank"
+        >
+            <button>
+                Watch Officially
+            </button>
+        </a>
+
+    ;`
+
+}
+
+fetchMovie();
+
+
+
+
+// BACK BUTTON
+const backBtn = document.querySelector("#backBtn");
+
+backBtn.addEventListener("click", () => {
+
+    window.location.href = "index.html";
+
+});
